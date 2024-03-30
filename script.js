1a31701fa092e762aa72f7b95fc2d43144d0faf6
evanbrummer <ebrummer@iastate.edu>
2024-03-30 18:57:08 -0500

    Write user story concept

--- script.js
+++ script.js
@@ -19,11 +19,33 @@ async function fetchData() {
 }
 
 async function identifyCurrentService() {
-    let queryOptions = { active: true, currentWindow: true };
+    let queryOptions = { active: true, lastFocusedWindow: true };
     let [tab] = await chrome.tabs.query(queryOptions);
 
-    let serviceUrl = tab.url.split('/')[2]; // on account of https://
+    let serviceUrl = tab.url.split('/')[2]; // index 2, on account of https:// ... /
 
     document.getElementById("services").innerHTML = `<li>${serviceUrl} (${SUPPORTED_STREAMERS.includes(serviceUrl)})</li>`;
 }
 identifyCurrentService();
+
+
+/**
+ * ---- STORIES ----
+ * CLICKED:
+ * 1) check what service they have open
+ * 2) it's not recognized/supported
+ * 3) message: not supported, click here for supported streaming services
+ * 
+ * 
+ * CLICKED:
+ * 1) check what service they have open
+ * 2) it's recognized (disney, netflix, whatever)
+ * 3) app name and favico shown on ext with a toggle switch to "Select multiple languages"
+ *      - maybe an extra checkbox to "automatically set text to smallest option"
+ * 4) opening the CC/subtitles menu of the player looks and functions the same, but you can check/select more than one
+ *      - they will show vertically in the order selected, and maybe be marked as (1) English ... (2) Spanish
+ *      - "Are you not entertained?"
+ *      - "¿No estás entretenido?"
+ * 
+ * the devil's in the details 
+ */
